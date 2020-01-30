896e746cca792925c0b4e09e349a8e85f5524977
Patrick Tweddell <patrick.tweddell@mail.mcgill.ca>
2020-01-30 18:26:48 -0500

    backend spring boot initiated

---
+++ McFantasy-Backend/src/test/java/ca/mcgill/ecse428/mcFantasy/McFantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse428.mcFantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McFantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
