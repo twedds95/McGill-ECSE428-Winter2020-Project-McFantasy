e59aa11756d189c34a4be846b389aa64834a8f71
Brendan Marks <brendan99@live.ca>
2020-02-02 16:20:26 -0500

    work on services and rest controller

--- McFantasy-Backend/src/main/java/ca/mcgill/ecse428/mcFantasy/model/AppUser.java
+++ McFantasy-Backend/src/main/java/ca/mcgill/ecse428/mcFantasy/model/AppUser.java
@@ -11,59 +11,59 @@ import javax.persistence.OneToMany;
 
 @Entity
 public class AppUser{
-   private String email;
+	private String email;
 
-public void setEmail(String value) {
-    this.email = value;
-}
-@Id
-public String getEmail() {
-    return this.email;
-}
-private String name;
+	public void setEmail(String value) {
+		this.email = value;
+	}
+	@Id
+	public String getEmail() {
+		return this.email;
+	}
+	private String name;
 
-public void setName(String value) {
-    this.name = value;
-}
-public String getName() {
-    return this.name;
-}
-private String password;
+	public void setName(String value) {
+		this.name = value;
+	}
+	public String getName() {
+		return this.name;
+	}
+	private String password;
 
-public void setPassword(String value) {
-    this.password = value;
-}
-public String getPassword() {
-    return this.password;
-}
-private Blob profilePicture;
+	public void setPassword(String value) {
+		this.password = value;
+	}
+	public String getPassword() {
+		return this.password;
+	}
+	private byte[] profilePicture;
 
-public void setProfilePicture(Blob value) {
-    this.profilePicture = value;
+	public void setProfilePicture(byte[] value) {
+		this.profilePicture = value;
+	}
+	public byte[] getProfilePicture() {
+		return this.profilePicture;
+	}
+	private Set<Team> team;
+
+	@OneToMany(mappedBy="user" , cascade={CascadeType.ALL})
+	public Set<Team> getTeam() {
+		return this.team;
+	}
+
+	public void setTeam(Set<Team> teams) {
+		this.team = teams;
+	}
+
+	private Set<League> league;
+
+	@OneToMany(mappedBy="user" , cascade={CascadeType.ALL})
+	public Set<League> getLeague() {
+		return this.league;
+	}
+
+	public void setLeague(Set<League> leagues) {
+		this.league = leagues;
+	}
+
 }
-public Blob getProfilePicture() {
-    return this.profilePicture;
-}
-   private Set<Team> team;
-   
-   @OneToMany(mappedBy="user" , cascade={CascadeType.ALL})
-   public Set<Team> getTeam() {
-      return this.team;
-   }
-   
-   public void setTeam(Set<Team> teams) {
-      this.team = teams;
-   }
-   
-   private Set<League> league;
-   
-   @OneToMany(mappedBy="user" , cascade={CascadeType.ALL})
-   public Set<League> getLeague() {
-      return this.league;
-   }
-   
-   public void setLeague(Set<League> leagues) {
-      this.league = leagues;
-   }
-   
-   }
